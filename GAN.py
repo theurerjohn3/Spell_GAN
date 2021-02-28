3416b165921415b9070985542cb2a4f8125fb3d1
Theurer <john.theurer@cshs.org>
2021-02-27 18:12:05 -0800

    First commit

---
+++ GAN.py
@@ -0,0 +1,8 @@
+from keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model, load_model
+from keras.optimizers import Adam
+import numpy as np
+from PIL import Image
+import os
